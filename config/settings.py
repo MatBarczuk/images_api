6e0487c8716c417f28546189851820d227f24e2b
Mateusz Barczuk <matbarczuk@gmail.com>
2021-07-27 21:25:52 +0200

    add functionalities for images

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Third-party
-    'rest-framework',
+    'rest_framework',
 
     # Local
     'users.apps.UsersConfig',
@@ -147,3 +147,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+IMAGE_TYPES = ['jpeg', 'png']
